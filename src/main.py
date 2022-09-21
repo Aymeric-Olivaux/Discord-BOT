b1e527133fbae7f472c84d89ab792b1d68239ee5
Aymeric-Olivaux <aymeric.olivaux@epita.fr>
2022-09-21 19:59:48 +0200

    [ADD] WarmUp Name and token handling

--- src/main.py
+++ src/main.py
@@ -1,25 +1,37 @@
 from discord.ext import commands
+from dotenv import load_dotenv
 import discord
+import os
+
+load_dotenv(dotenv_path="../config")
+os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
-bot = commands.Bot(
+client = commands.Bot(
     command_prefix="!",  # Change to desired prefix
     case_insensitive=True, # Commands aren't case-sensitive
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+client.author_id = 436535678185111553  # Change to your discord id
 
-@bot.event
+@client.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
-    print(bot.user)  # Prints the bot's username and identifier
+    print(client.user)  # Prints the bot's username and identifier
 
-@bot.command()
-async def pong(ctx):
-    await ctx.send('pong')
+@client.command(name="name")
+async def name_back(ctx):
+    """
+    When you do "!name" command, return the name of whom write the command
+    :param ctx:
+    :return:
+    """
+    general_channel = client.get_channel(1022194900982308977)
+    await general_channel.send(ctx.author)
+    print(ctx.author)
 
-token = "<MY_TOKEN>"
-bot.run(token)  # Starts the bot
+
+client.run(os.getenv("TOKEN"))  # Starts the bot
